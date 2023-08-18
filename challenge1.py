b37401cb62d3d58583c5c52b94cef85892e61382
judy-chelangat <judysigilai75@gmail.com>
2023-08-19 00:09:27 +0300

    added an if statement to check if its 12

--- challenge1.py
+++ challenge1.py
@@ -5,7 +5,12 @@
 def time(hour,min,period):
     new_hour = hour+12 
     new_min =min+00
-    return (f"{new_hour}{new_min} hrs")
+    #if statement to check whether its' noon or midnight
+    if hour == 12 and min == 00:
+        return (f"{hour}{min}0 {period}")
+    else:
+      return (f"{new_hour}{new_min} hrs")
 
 
-print(time(10,24,"am"))
+print(time(12,00,"am"))
+
