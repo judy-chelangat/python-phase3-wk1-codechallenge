c31913df7cdbf43238752fc3f4825766c287b596
judy-chelangat <judysigilai75@gmail.com>
2023-08-19 19:39:46 +0300

    initial commit

---
+++ challenge2.py
@@ -0,0 +1,3 @@
+#function, which takes three integers a, b, and c as arguments,
+# and returns True if exactly two of of the three integers are
+ #positive numbers (greater than zero), and False - otherwise.
