5d9783ed43a2cd6ce1e9bfe332fe3ab14dc33cf3
judy-chelangat <judysigilai75@gmail.com>
2023-08-19 21:08:02 +0300

    implemented a function to check for the highest value of a string

--- challenge3.py
+++ challenge3.py
@@ -2,3 +2,17 @@
 #return the highest value of consonant substrings.
 # Consonants are any letters of the alphabet except "aeiou".
 #We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+def highest_value(string):
+    max_value=0  #value to keep track of max value
+    consonants ='bcdfghjklmnpqrstvwxyz'
+    number_value =0 
+    for letter in string:
+        if letter in consonants: #checking if the letters are in the consonants
+         number_value += ord(letter) - ord('a') + 1
+        if number_value > max_value:
+                max_value = number_value
+        else:
+            number_value =0
+
+            return number_value
