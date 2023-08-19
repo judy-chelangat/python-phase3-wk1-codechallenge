5da9e379567cb3edb8d44ea89f75d75b448dd4f0
judy-chelangat <judysigilai75@gmail.com>
2023-08-19 19:49:16 +0300

    defined a function to check if the numbers are positive

--- challenge2.py
+++ challenge2.py
@@ -1,3 +1,18 @@
 #function, which takes three integers a, b, and c as arguments,
 # and returns True if exactly two of of the three integers are
  #positive numbers (greater than zero), and False - otherwise.
+
+def number_checker(a,b,c):
+    if a>0 and b>0:
+        return 'True'
+    elif a>0 and c>0:
+        return 'True'
+    elif b>0 and c>0:
+        return 'True'
+    elif b>0 and a>0:
+        return 'True'
+    else:
+        return 'False'
+    
+    
+print(number_checker(2,4,-5))
